a6af650e1f212ba10190cf33ff5f62344f8c93c1
Satish Kapalavayi <satishkapalavayi@gmail.com>
2017-05-24 17:14:41 -0400

    Splitting Training and Test sets - Done

--- ML/Data_Preprocessing/data_preprocessing_template.py
+++ ML/Data_Preprocessing/data_preprocessing_template.py
@@ -25,3 +25,10 @@ onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
+
+
+#Train Test data splitting
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
+
+
