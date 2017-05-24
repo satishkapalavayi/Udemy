1e96b3a8dbf60d7b69e1cec019fedabe899f05f5
Satish Kapalavayi <satishkapalavayi@gmail.com>
2017-05-24 16:26:56 -0400

    How to deal with Missing values in python and R

--- ML/Data_Preprocessing/data_preprocessing_template.R
+++ ML/Data_Preprocessing/data_preprocessing_template.R
@@ -2,3 +2,11 @@
 
 #Importing the dataset
 dataset = read.csv("Data.csv")
+
+#Missing data
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
--- ML/Data_Preprocessing/data_preprocessing_template.py
+++ ML/Data_Preprocessing/data_preprocessing_template.py
@@ -10,3 +10,9 @@ import pandas as pd
 dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+#Missing Data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = "NaN", strategy = "mean", axis =0)
+imputer = imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
