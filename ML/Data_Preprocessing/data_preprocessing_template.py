3eb12f3c32a04bb4629c3baf2abfafd36cc38184
Satish Kapalavayi <satishkapalavayi@gmail.com>
2017-06-02 09:55:02 -0400

    Modified

--- ML/Data_Preprocessing/data_preprocessing_template.py
+++ ML/Data_Preprocessing/data_preprocessing_template.py
@@ -39,3 +39,4 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 
+
--- ML/Regression/Simple_Linear_Regression/Simple_Linear_Regression.py
+++ ML/Regression/Simple_Linear_Regression/Simple_Linear_Regression.py
@@ -17,7 +17,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
